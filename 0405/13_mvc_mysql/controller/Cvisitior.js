c5c8e7f1c31fdcbf5401ddfd28ad56148e32f221
chalkos-eche <smurf@kakao.com>
2023-04-05 20:06:31 +0900

    studying : mysql connected with nodejs

--- 0405/13_mvc_mysql/controller/Cvisitior.js
+++ 0405/13_mvc_mysql/controller/Cvisitior.js
@@ -1,5 +1,6 @@
 const Visitor = require("../model/Visitor");
 
+//(1) GET / =>
 exports.main = (req, res) => {
   // 인덱스화면 출력
   res.render("index");
@@ -7,7 +8,17 @@ exports.main = (req, res) => {
 
 //get /visitor = localhost:Port/visitor
 
+// (2) Get /visitor =>
 exports.getVisitors = (req, res) => {
-  console.log(Visitor.getVisitors());
-  res.render("visitor", { data:Visitor.getVisitors() });
+  //
+  // [before] 이전 코드
+  // console.log(Visitor.getVisitors());
+  // res.render("visitor", { data:Visitor.getVisitors() });
+  //
+  //  [after] mysql db  연결 ;
+  Visitor.getVisitors((result) => {
+    console.log("Cvisitor.js >>", result);
+    res.render("visitor", { data:result });
+    ;
+  });
 };
--- 0405/13_mvc_mysql/model/Visitor.js
+++ 0405/13_mvc_mysql/model/Visitor.js
@@ -1,8 +1,43 @@
-exports.getVisitors = () =>{
-  return [
-    {id:1,name:'홍길동',comment:'내가왔다.'},
-    {id:2,name:'이찬혁',comment:'으라차차'},
-  ]
-}
+// exports.getVisitors = () => {
+//   return [
+//     {
+//       id:1,
+//       name:"홍길동",
+//       comment:"내가왔다."
+//     },
+//     {
+//       id:2,
+//       name:"이찬혁",
+//       comment:"으라차차"
+//     }
+//   ];
+// };
 
 // MySQL 연결 전 상태
+// [after] mysql 연결;
+const mysql = require("mysql");
+
+// database 연결
+const conn = mysql.createConnection({
+  host:"localhost", // database 가 설치된 IP 주소
+  // port:3306,
+  user:"user", // mysql user 계정의 아이디
+  password:"1234", //데이터베이스 접속 비밀번호
+  database:"kdt"
+});
+
+exports.getVisitors = (cb) => {
+  // conn.query(sql,callback);
+  //  sql : sql 문 // calback : 할 일
+  // => conn 에 저장되어 있는 데이터베이스 접근해서 sql문 실행 => 결과를 callback 함수에 넘겨줌
+  // conn.query(); // .query() 메소드 존재
+  const sql = "SELECT * FROM visitor;";
+
+  conn.query(sql, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    console.log("Visitor.js >>", rows);
+    cb(rows);
+  });
+};
