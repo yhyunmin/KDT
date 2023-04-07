e5b14568e84fa74c257b3fdb0c94ebde8f67ca06
chalkos-eche <smurf@kakao.com>
2023-04-07 19:06:48 +0900

    로그인 / 회원가입 /정보수정 /회원탈퇴 폼 실습완료

--- 0406/실습/controller/Cmain.js
+++ 0406/실습/controller/Cmain.js
@@ -1,31 +1,114 @@
-const Model = require('../model/Model');
+const Model = require("../model/Model");
 
 // 메인화면
 exports.getIndex = (req, res) => {
-  res.render('index', {
-    pageTitle: '사이트 이름',
+  res.render("index", {
+    pageTitle: "사이트 이름",
   });
 };
 
 // get 로그인 화면
 exports.getLogin = (req, res) => {
-  res.render('login', {
-    pageTitle: '로그인 페이지 이름',
+  res.render("login", {
+    pageTitle: "로그인 페이지 이름",
   });
-  Model.getLogin();
 };
+exports.postCheckLogin = (req, res) => {
+  console.log("req.body :", req.body);
+  Model.postCheckLogin(req.body, (rows) => {
+    // console.log("rows", rows);
+    // console.log("id check?", req.body.userId, rows.userid);
+    // console.log("pw check?", req.body.userPw, rows.pw);
+    console.log("***", rows);
 
+    // 데이터 베이스에서 빈값일때 에러뜸
+    if (!rows) {
+      return res.send({
+        hasInfo: false,
+      });
+    } else if (req.body.userId === rows.userid || req.body.userPw === rows.pw) {
+      res.send({
+        hasInfo: true,
+      });
+    }
+  });
+  // 내 정보가서 사용할 수 잇나 ?
+  return req.body;
+};
+// post 내정보 화면
+exports.postMyProfile = (req, res) => {
+  console.log("req.body", req.body); // id pw 저장되어있음
+  Model.postMyProfile(req.body, (rows) => {
+    console.log("rows cmain", rows);
+    console.log("rows.name", rows.name);
+    res.render("my_profile", {
+      pageTitle: "내 정보",
+      userName: rows.name,
+      userId: rows.userid,
+      userPw: rows.pw,
+      id: rows.id,
+    });
+  });
+};
+// patch 내정보 수정
+exports.patchUserInfo = (req, res) => {
+  console.log(" req.body", req.body);
+  Model.patchUserInfo(req.body, (rows) => {
+    console.log("rows Cmain 111", rows.name);
+    console.log("rows Cmain 222", req.body.name);
+    res.send({
+      hasSuccess: true,
+      newName: req.body.name,
+    });
+  });
+};
 // get 회원가입 사이트
 exports.getRegister = (req, res) => {
-  res.render('register', {
-    pageTitle: '회원가입 페이지 이름',
+  res.render("register", {
+    pageTitle: "회원가입 페이지 이름",
   });
-  Model.getRegister();
+};
+// post 회원가입 결과
+exports.getResult = (req, res) => {
+  console.log("Cmain.js gerResult:", req.body);
+  Model.getResult(req.body);
+  res.render("result");
+};
+// post 아이디 유효성 확인
+exports.postCheckUserId = (req, res) => {
+  console.log("requested req.body : ", req.body);
+  console.log("requested req.body.id : ", req.body.id);
+  // const realData = Model.dummy();
+  Model.postCheckUserId(req.body, (rows) => {
+    console.log("Cmain.js userInfo rows : ", rows);
+    // indexOf는 배열에서 값찾는거 // findIndex : 객체에서 값찾는거 .findIndex(item => item.userid === '1');
+    // 유저 아이디 배열화
+    const idx = rows.map((row) => row.userid);
+    console.log("idx.userid", idx);
+    // 유저 아이디 중에서 요청한 아이디(회원가입한 아이디) 찾기
+    const idx2 = idx.indexOf(req.body.id);
+    console.log("idx2", idx2);
+    if (idx2 >= 0) {
+      // 아이디 있음
+      res.send({
+        hasId: true,
+        errorText: "사용할 수 없는 아이디입니다.",
+      });
+    } else if (idx2 < 0) {
+      // 아이디 없음
+      res.send({
+        hasId: false,
+        errorText: "사용할 수 있는 아이디입니다.",
+      });
+    }
+  });
+
+  // res.send(req.body.);
 };
 
-// get 회원 정보 수정
-exports.getMyProfile = (req, res) => {
-  res.render('my_profile', {
-    pageTitle: '내 정보',
+exports.deleteUserInfo = (req, res) => {
+  Model.deleteUserInfo(req.body, (rows) => {
+    console.log(rows);
+    res.send("성공!");
   });
 };
--- 0406/실습/routes/index.js
+++ 0406/실습/routes/index.js
@@ -1,17 +1,26 @@
-const express = require('express');
-const controller = require('../controller/Cmain');
+const express = require("express");
+const controller = require("../controller/Cmain");
 const router = express.Router();
 
 //메인 화면
-router.get('/', controller.getIndex);
+router.get("/", controller.getIndex);
 
 //로그인 화면
-router.get('/login', controller.getLogin);
+router.get("/login", controller.getLogin);
+//로그인 아이디 확인
+router.post("/checkLogin", controller.postCheckLogin);
 
 //회원가입 화면
-router.get('/register', controller.getRegister);
+router.get("/register", controller.getRegister);
+//회원가입 아이디 중복 검사
+router.post("/check_userid", controller.postCheckUserId);
+//회원가입 성공 화면
+router.post("/result", controller.getResult);
 
 //내정보 화면
-router.get('/my_profile', controller.getMyProfile);
+router.post("/my_profile", controller.postMyProfile);
+//내정보 수정 화면
+router.patch("/patchUserInfo", controller.patchUserInfo);
 
+router.delete("/my_profile/delete", controller.deleteUserInfo);
 module.exports = router;
