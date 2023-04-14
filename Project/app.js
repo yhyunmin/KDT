27da3e4b422d320942dad12fa654ba63602237c0
chalkos-eche <smurf@kakao.com>
2023-04-14 21:58:51 +0900

    개인프로젝트 : ㅁㄹ?

--- Project/app.js
+++ Project/app.js
@@ -12,7 +12,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // body-parser
-app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
@@ -23,25 +23,54 @@ app.use("/static", express.static(__dirname + "/static"));
 const indexRouter = require("./routes/index");
 
 app.use("/", indexRouter);
-
+const nickObj = {};
+function updateNickList() {
+  io.emit("updateNicks", nickObj);
+  // 서버에 접속한 클라이언트들에게 nickObj 에 변경이 일어났음을 알리는 이벤트
+}
 // 채팅방 입장
 io.on("connection", (socket) => {
   console.log("a user connected socket.id =>", socket.id);
-  // react test
-  socket.on("react-quake", (reactID, target) => {
+  // 닉네임 검사
+  socket.on("setNick", (nick) => {
+    if (Object.values(nickObj).indexOf(nick) > -1) {
+      socket.emit("error", "이미 존재하는 닉네임입니다.");
+    } else {
+      //아이디 통과
+      nickObj[socket.id] = nick;
+      io.emit("notice", `${nick}님이 입장하셨습니다.`);
+      socket.emit("entry", nick);
+      updateNickList();
+    }
+  });
+  socket.on("disconnect", () => {
+    console.log(nickObj);
+    console.log(socket.id);
+    console.log(nickObj[socket]);
+    io.emit("notice", `${nickObj[socket.id]}님이 퇴장하셨습니다.`);
+    delete nickObj[socket.id];
+    updateNickList();
+  });
+
+  // 반응 주고 받기
+  socket.on("quake", (data) => {
+    console.log(data);
+    const obj = {
+      first: data.reactID[0],
+      second: data.reactID[1],
+      id: data.id,
+    };
+    io.emit("react", obj);
+  });
+  socket.on("react-tada", (reactID, target) => {
     console.log(reactID, target);
     io.emit("react", reactID, target);
   });
-  socket.on("react-tada", (reactID, target) => {
-        console.log(reactID, target);
-        io.emit("react", reactID, target);
-      }
-  );
 });
 const userNames = {};
 // 채팅방 대화 송수신
 //
 
 http.listen(PORT, () => {
-  console.log(`http://localhost:${ PORT }`);
+  console.log(`http://localhost:${PORT}`);
 });
